065badb233f11c5ba65ddb17f06d62a209ffb879
Bayugoon <45611382+Bayugoon@users.noreply.github.com>
2021-11-13 15:23:20 -0400

    changed to correct token

--- tokens.py
+++ tokens.py
@@ -1,3 +1,3 @@
 
 # Current using the '''Minecraft Discontinued Features Bot''' token
-DISCORD_TOKEN = "08R2h4juGbzd_5SHhpPo_v7_zL_96Fpy"
+DISCORD_TOKEN = "NjMxMjkzMTg1MTcxMTI4MzQx.XZ0vmA.J1hfH7pb4tP5kFLI3ozHy2KtXe4"
