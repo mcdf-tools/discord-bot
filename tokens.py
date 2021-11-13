09f4b3b8147e5c0445bae3095de0d819b714865c
molneya <62799417+molneya@users.noreply.github.com>
2021-11-14 04:14:40 +0800

    im stupid

--- tokens.py
+++ tokens.py
@@ -1,4 +1,3 @@
 
 # Current using the '''Minecraft Discontinued Features Bot''' token
-#DISCORD_TOKEN = "NjMxMjkzMTg1MTcxMTI4MzQx.XZ0vmA.J1hfH7pb4tP5kFLI3ozHy2KtXe4"
-DISCORD_TOKEN = "OTAwMjQxMzE1NTMzNTc0MTk0.YW-dDw.AHKg72Efz7P6ZTIfddxtg7AJSlg"
+DISCORD_TOKEN = "NjMxMjkzMTg1MTcxMTI4MzQx.XZ0vmA.J1hfH7pb4tP5kFLI3ozHy2KtXe4"
