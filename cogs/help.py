1cdaeb3d3022061b60387e3f6dfa4c7d51ec9991
molneya <62799417+molneya@users.noreply.github.com>
2021-11-15 22:18:55 +0800

    split help into help and info

--- cogs/help.py
+++ cogs/help.py
@@ -5,6 +5,7 @@ import discord, json
 class cog_help(commands.Cog):
 
     def __init__(self, client):
+        
         self.client = client
         self.help = json.load(open(f"help.json", 'r'))
 
@@ -45,27 +46,7 @@ class cog_help(commands.Cog):
                     embedVar.add_field(name="Optional Arguments", value=help['optional'], inline=False)
 
                     await ctx.send(embed=embedVar)
-        
-        
-    @commands.command(name="wiki", aliases=["info"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def wiki(self, ctx):
 
-        # Create embed
-        embedVar = discord.Embed(color=self.embed_color, title="MC Disontinued Wiki Info")
-        embedVar.set_thumbnail(url=self.thumbnail_image)
-        embedVar.set_footer(text=self.footer_text)
-
-        # Add fields
-        embedVar.add_field(name="Wiki", value="https://mcdiscontinued.miraheze.org", inline=False)
-        embedVar.add_field(name="Discord", value="https://discord.gg/n7v7pgE", inline=False)
-        embedVar.add_field(name="Test Wiki", value="https://mcdftestingwiki.miraheze.org", inline=False)
-        embedVar.add_field(name="Github", value="https://github.com/Minecraft-Discontinued-Features", inline=False)
-        #embedVar.add_field(name="Youtube", value="https://www.youtube.com/channel/UC8gg3Ak2a8PO0iiwv9I2eNA (plans for this account are currently on hold)", inline=False)
-        embedVar.add_field(name="Reddit", value="https://www.reddit.com/r/DiscontinuedMC", inline=False)
-        
-        await ctx.send(embed=embedVar)
-        
 
 def setup(bot):
     bot.add_cog(cog_help(bot))
