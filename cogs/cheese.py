84b817a516a5f6e849ae0442982a21fd1319573a
Bayugoon <45611382+Bayugoon@users.noreply.github.com>
2022-09-03 16:35:21 -0300

    Update cheese.py

--- cogs/cheese.py
+++ cogs/cheese.py
@@ -9,7 +9,7 @@ class cog_cheese(commands.Cog):
         
     @commands.command(name="cheese", aliases=["ILoveCheese"])
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def ILoveCheese(self, ctx, member):
+    async def ILoveCheese(self, ctx):
       member = ctx.message.author
       role = ctx.guild.get_role(802013239289970690)
       if role in member.roles:
